d27c3ad8c5129de8cca8707dad7fd64582d60c51
WillLe2502 <quangnhatminhle25021998@gmail.com>
2022-11-14 00:20:11 +1100

    productList

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
 
-	$('#slides').superslides({
-		animation: 'fade',
-		play: 5000,
-		pagination: false,
+	var typed = new Typed(".typed",{
+		strings: ["From Book Lovers", "To Book Lovers"],
+		typeSpeed: 50,
+		loop: true,
+		startDelay: 1200,
+		showCursor: false,
+		backSpeed: 35
 	});
 
 });
